b06d31e614885a2aae30d5a8997bcc6132e9e9f7
idmossab <idmossab@gmail.com>
2025-12-29 23:20:19 +0000

    start

--- src/main/java/com/example/_blog/Entity/Blog.java
+++
@@ -1,5 +0,0 @@
-package com.example._blog.Entity;
-
-public class Blog {
-    
-}
--- src/main/java/com/example/_blog/Entity/User.java
+++ src/main/java/com/example/_blog/Entity/User.java
@@ -1,14 +1,11 @@
 package com.example._blog.Entity;
 
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -26,8 +23,5 @@ public class User {
 
     @Column(name = "password", nullable = false)
     private String password;
-
-    @OneToMany(mappedBy = "user" , cascade = CascadeType.ALL , orphanRemoval = true)
-    private List<Blog> blogs;
 
 }
