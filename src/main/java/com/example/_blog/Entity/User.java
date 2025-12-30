8ca3492c728be80b37114ea68267660d169ee2d7
idmossab <idmossab@gmail.com>
2025-12-30 13:17:24 +0000

    entitiy user

--- src/main/java/com/example/_blog/Entity/User.java
+++ src/main/java/com/example/_blog/Entity/User.java
@@ -1,8 +1,13 @@
 package com.example._blog.Entity;
 
 
+import com.example._blog.Entity.enums.UserRole;
+import com.example._blog.Entity.enums.UserStatus;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -15,13 +20,23 @@ import lombok.Data;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column( nullable = false, unique = true)
-    private String username;
-    @Column(name = "email", nullable = false, unique = true)
+    private int id;
+    @Column(nullable=false)
+    private String firstName;
+    @Column(nullable=false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String userName;
+    @Column(nullable = false, unique = true)
     private String email;
-
-    @Column(name = "password", nullable = false)
+    @Column(nullable = false)
     private String password;
-
+    private String bio;
+    private String avatar;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status=UserStatus.ACTIVE;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role=UserRole.USER;
 }
